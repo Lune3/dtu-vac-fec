f6481a69bcbfe8a65205f10ae256246984b9d45a
Lune3 <113820374+Lune3@users.noreply.github.com>
2024-10-26 14:45:20 +0530

    Extraction of courseId

--- backend/Controllers/fileIndexController.ts
+++ backend/Controllers/fileIndexController.ts
@@ -3,10 +3,10 @@ import { PdfReader } from "pdfreader";
 import prisma from "../db";
 
 const pdfReader = new PdfReader();
-function parsePdf() {
+function parsePdf(path : string) {
     return new Promise((resolve, reject) => {
         const pdfContent: string[] = [];
-        pdfReader.parseFileItems("../public/file0806.pdf", (err, item) => {
+        pdfReader.parseFileItems(path, (err, item) => {
             if (err) {
                 reject(err);
             } else if (!item) {
@@ -29,6 +29,29 @@ const addCoursesToDb = async (courseArray: Array<string>) => {
     }
 }
 
+function isElectiveId(extractId : string) : boolean{
+    const courseIdPattern = /(CE|CH|MC|CS|CO|SE|ME|PE|AE|EN|EP|EE|EC|BT|IT|MGT|MGS|MGM|NST|MSPH|BBA|DD|FB|FIE|FSE|MGK|HU|DD)[\s\-]?\d{3}/g;
+
+    const firstResult = courseIdPattern.test(extractId);
+    if(firstResult === true)return true;
+    return false;
+}
+
+
+function extractElectives(electivePdf : string) : Array<string> {
+    const electivesArray : string[] = [];
+    
+    for(let i = 0;i < electivePdf.length - 6;i++){
+        const extractId = electivePdf.slice(i,i + 6).trimEnd();
+        const checkElectiveId : boolean = isElectiveId(extractId);
+        if(checkElectiveId){
+            electivesArray.push(extractId);
+        }
+    }
+
+    return electivesArray;
+}
+
 export const readPdf = async (req: Request, res: Response, next: NextFunction) => {
     let coursesCount: number = -1;
     try {
@@ -37,7 +60,7 @@ export const readPdf = async (req: Request, res: Response, next: NextFunction) =
         console.log(error);
     }
     if (coursesCount === 0) {
-        // let pdf = await parsePdf();
+        // let fecPdf = await parsePdf("../pdfFile/file0806.pdf");
         // if (typeof (pdf) === "string") {
         //     const coursePattern = /FEC\d{1,2}[\s\S]*?(?=FEC|S[1-4]|\sO\s)/g;
         //     const courses = pdf.match(coursePattern);
@@ -53,10 +76,17 @@ export const readPdf = async (req: Request, res: Response, next: NextFunction) =
         // else {
         //     res.status(500).send("File Read Error");
         // }
-        console.log("yepeee");
+        let electivePdf = await parsePdf("./pdfFile/electives.pdf");
+        if(typeof(electivePdf) === 'string'){
+            const electiveCode = extractElectives(electivePdf);
+            console.log(electiveCode);
+        }
+        res.send(`${electivePdf}`);
     }
     else {
         next();
     }
 };
+
+
 
