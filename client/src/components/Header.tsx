303fe4e9fe64c1370a9c36697ea1b86201d4ca31
Lune3 <113820374+Lune3@users.noreply.github.com>
2024-11-03 22:44:51 +0530

    Using useffect to fetch data in header

--- client/src/components/Header.tsx
+++ client/src/components/Header.tsx
@@ -1,56 +1,67 @@
 import { apiUrl } from "../config";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 
 type HeaderProps = {
     setCourse: React.Dispatch<React.SetStateAction<string>>,
-    setComments : React.Dispatch<React.SetStateAction<object>>;
+    setComments: React.Dispatch<React.SetStateAction<object>>;
 };
 
 
-async function getComment(courseName : string){
-    const fetchComments = await fetch(`${apiUrl}/comment/${courseName}`,{mode:"cors"});
+async function getComment(courseName: string) {
+    const fetchComments = await fetch(`${apiUrl}/comment/${courseName}`, { mode: "cors" });
     const comments = await fetchComments.json();
     return comments;
-}   
+}
 
-function Header({setComments, setCourse}: HeaderProps){
-    const [courseName,setCourseName] = useState<string>("");
-    const courseSubmit : React.FormEventHandler<HTMLFormElement> = async (e) => {
+
+function Header({ setComments, setCourse }: HeaderProps) {
+    const [courseName, setCourseName] = useState<string>("");
+    useEffect(() => {
+        const courseSubmit = async () => {
+            const url = `${apiUrl}/course/${courseName}`;
+            const getCourse = await fetch(url, { mode: "cors" });
+            if (getCourse.status === 200) {
+                const course = await getCourse.json();
+                setCourse(course.course.name);
+                const comments = await getComment(course.name);
+                setComments(comments);
+            }
+            else if (getCourse.status === 422) {
+                console.log("wrong");
+            }
+            else {
+                console.log("very wrong");
+            }
+        }
+        courseSubmit();
+        return () =>{
+            setCourseName("");
+        }
+    }, [courseName]);
+
+    const foo: React.FormEventHandler<HTMLFormElement> = (e) => {
         e.preventDefault();
-        const url = `${apiUrl}/course/${courseName}`;
-        const getCourse = await fetch(url,{mode:"cors"});
-        if(getCourse.status === 200){
-            const course = await getCourse.json(); 
-            setCourse(course.course.name);
-            const comments = await getComment(course.name);
-            setComments(comments);
-        }
-        else if(getCourse.status === 422){
-            console.log("wrong");
-        }
-        else{
-            console.log("very wrong");
-        }
+        const courseData = e.target as HTMLFormElement;
+        setCourseName((courseData.elements[0] as HTMLInputElement).value);
     }
-
 
     return (
         <header className="flex justify-between p-3 border-b border-black">
             <div>
                 <h1>Dtu electives and fec</h1>
             </div>
-            <form action="" method="GET" onSubmit={courseSubmit}>
-                <input type="text" placeholder="Search for a course" maxLength={50} className="border border-black" name="courseName" onChange={(e) => setCourseName(e.target.value)}/>
-                <button type="submit" style={{display:"none"}}></button>
+            <form action="" method="GET" onSubmit={foo}>
+                <input type="text" placeholder="Search for a course" maxLength={50} className="border border-black" name="courseName" />
+                <button type="submit" style={{ display: "none" }}></button>
             </form>
             <div>
                 <button>Log In</button>
                 <picture>
-                    change theme 
+                    change theme
                 </picture>
             </div>
         </header>
     )
 }
 
-export {Header}
+export { Header }
